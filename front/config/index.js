42f1c0e9a1821a19dee3461c36900e23ff6e09f5
유승민 <yu3096@gmail.com>
2021-11-03 13:25:58 +0900

    express - vue Setting

--- front/config/index.js
+++ front/config/index.js
@@ -45,10 +45,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    //index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../back/public/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    //assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../../back/public'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
